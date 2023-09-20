f281a663dbbd7a3be75c70f033354c6a1d96a629
Alex Litchfield <alexblitchfield@gmail.com>
2023-09-20 14:52:53 -0400

    Variable Type changes

--- autofill/autofillScript.ts
+++ autofill/autofillScript.ts
@@ -1,8 +1,12 @@
 // This file will be run on the content page, and should include all the logic for inserting text into form items.
-const usernameField = document.querySelector('input[name="username"]');
-const passwordField = document.querySelector('input[name="password"]');
+const firstNameField = document.querySelector('input[name="First Name"]');
+const lastNameField = document.querySelector('input[name="Last Name"]');
+const emailField = document.querySelector('input[name="Email"]');
+//Figure out how to do a phone number field
 
-if (usernameField) (usernameField as HTMLInputElement).value = "yourUsername";
-if (passwordField) (passwordField as HTMLInputElement).value = "yourPassword";
+if (firstNameField) (firstNameField as HTMLInputElement).value = "yourFirstName";
+if (lastNameField) (lastNameField as HTMLInputElement).value = "yourLastName";
+if (emailField) (emailField as HTMLInputElement).value = "yourEmail";
+//Figure out how to do a phone number field
 
 export {}
