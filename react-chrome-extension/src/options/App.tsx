c8db409c49fc284e6902bf5449a025e03745e852
Katijino <143554078+Katijino@users.noreply.github.com>
2023-09-22 17:10:08 -0400

    New options
    Added new options for further information and dropdown menus for some options.

--- react-chrome-extension/src/options/App.tsx
+++ react-chrome-extension/src/options/App.tsx
@@ -1,11 +1,42 @@
 // This app is the settings page where users can enter their info, and tweak general options about the extension
 
 import React from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
+import {Container, Form, Button, Dropdown, DropdownButton, ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
+import {useForm} from 'react-hook-form';
+import {useEffect, useState} from 'react';
 
+
+function LoadingButton() {
+    const [isLoading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        function simulateNetworkRequest() {
+            return new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+
+        if (isLoading) {
+            simulateNetworkRequest().then(() => {
+                setLoading(false);
+            });
+        }
+    }, [isLoading]);
+
+    const handleClick = () => setLoading(true);
+
+    return (
+        <button
+            className="btn btn-primary"
+            disabled={isLoading}
+            onClick={!isLoading ? handleClick : undefined}
+        >
+            {isLoading ? 'Loading…' : 'Submit'}
+        </button>
+    );
+}
+
+  
 function EasyAppOptions() {
-    const { register, handleSubmit } = useForm();
+    const {register, handleSubmit} = useForm();
 
     const onSubmit = (data: any) => {
         console.log(data);
@@ -14,7 +45,7 @@ function EasyAppOptions() {
     return (
         <Container>
             <h2 className="text-center mb-4">EasyApp Options</h2>
-            
+
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group>
                     <Form.Control type="text" placeholder="Your Name" {...register('name')} />
@@ -29,8 +60,22 @@ function EasyAppOptions() {
                 </Form.Group>
 
                 <Form.Group>
+                    <Form.Control as="textarea" placeholder="Skills" {...register('skills')} />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Control as="textarea" placeholder="Past Jobs/Experiences" {...register('jobs')} />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Control as="textarea"
+                                  placeholder="LinkedIn/Github/Personal Websites" {...register('links')} />
+                </Form.Group>
+
+                <Form.Group>
                     <Form.Control as="textarea" placeholder="Supplementary Text" {...register('otherQuestions')} />
                 </Form.Group>
+
 
                 <Form.Group>
                     <Form.Label>Upload Resume:</Form.Label>
@@ -46,8 +91,7 @@ function EasyAppOptions() {
                     <Form.Label>Upload CV:</Form.Label>
                     <Form.Control type="file" {...register('cv')} />
                 </Form.Group>
-
-                <Button type="submit" className="mt-4">Save Options</Button>
+                <LoadingButton/>
             </Form>
         </Container>
     );
