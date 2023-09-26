e995870a3acdfd35182518f64f0e9cafccac1d6f
Katijino <143554078+Katijino@users.noreply.github.com>
2023-09-26 14:24:06 -0400

    App.tsx settings page in react updated.
    Added additional supplementary forms, section for linkedin/github, past jobs/skills, and changed the submit button to "load" for a period of time after a user submits the information so the user feels like their submission went through.

--- react-chrome-extension/src/options/App.tsx
+++ react-chrome-extension/src/options/App.tsx
@@ -1,8 +1,39 @@
 // This app is the settings page where users can enter their info, and tweak general options about the extension
 
 import React from 'react';
-import { Container, Form, Button, Dropdown } from 'react-bootstrap';
-import { useForm} from 'react-hook-form';
+import { Container, Form, Button, Dropdown, DropdownButton, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import {useEffect, useState} from 'react';
+
+
+function LoadingButton() {
+    const [isLoading, setLoading] = React.useState(false);
+  
+    React.useEffect(() => {
+      function simulateNetworkRequest() {
+        return new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+  
+      if (isLoading) {
+        simulateNetworkRequest().then(() => {
+          setLoading(false);
+        });
+      }
+    }, [isLoading]);
+  
+    const handleClick = () => setLoading(true);
+  
+    return (
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={!isLoading ? handleClick : undefined}
+      >
+        {isLoading ? 'Loading…' : 'Submit'}
+      </button>
+    );
+  }
+
 
 function EasyAppOptions() {
     const { register, handleSubmit } = useForm();
@@ -37,43 +68,13 @@ function EasyAppOptions() {
                 </Form.Group>
 
                 <Form.Group>
-                    <Form.Label>Gender:</Form.Label>
-                    <Form.Check type="radio" label="Male" value="male" {...register('Gender')} />
-                    <Form.Check type="radio" label="Female" value="female" {...register('Gender')} />
-                    <Form.Check type="radio" label="Other" value="other" {...register('Gender')} />
+                    <Form.Control as="textarea" placeholder="LinkedIn/Github/Personal Websites" {...register('links')} />
                 </Form.Group>
 
-                
-                <Form.Group>
-                    <Form.Label>Disabled Status:</Form.Label>
-                    <Form.Check type="radio" label="Yes" value="yes" {...register('disabledStatus')} />
-                    <Form.Check type="radio" label="No" value="no" {...register('disabledStatus')} />
-                    <Form.Check type="radio" label="No Answer" value="no answer" {...register('disabledStatus')} />
-                </Form.Group>
-
-                
-                <Form.Group>
-                    <Form.Label>Veteran Status:</Form.Label>
-                    <Form.Check type="radio" label="Yes" value="yes" {...register('veteranStatus')} />
-                    <Form.Check type="radio" label="No" value="no" {...register('veteranStatus')} />
-                    <Form.Check type="radio" label="No Answer" value="no answer" {...register('veteranStatus')} />
-                </Form.Group>
-                
-                <Form.Group>
-                    <Form.Label>Ethnicity:</Form.Label>
-                    <Form.Check type="radio" label="White" value="white" {...register('ethnicity')} />
-                    <Form.Check type="radio" label="Black" value="black" {...register('ethnicity')} />
-                    <Form.Check type="radio" label="Asian" value="asian" {...register('ethnicity')} />
-                    <Form.Check type="radio" label="Arab" value="arab" {...register('ethnicity')} />
-                    <Form.Check type="radio" label="Hispanic/Latino" value="hispanic-latino" {...register('ethnicity')} />
-                    <Form.Check type="radio" label="Mixed or Multiple ethnic groups" value="multiple" {...register('ethnicity')} />
-                    <Form.Check type="radio" label="No Answer" value="no answer" {...register('ethnicity')} />
-                </Form.Group>
-
-                
                 <Form.Group>
                     <Form.Control as="textarea" placeholder="Supplementary Text" {...register('otherQuestions')} />
                 </Form.Group>
+
 
                 <Form.Group>
                     <Form.Label>Upload Resume:</Form.Label>
@@ -89,8 +90,7 @@ function EasyAppOptions() {
                     <Form.Label>Upload CV:</Form.Label>
                     <Form.Control type="file" {...register('cv')} />
                 </Form.Group>
-
-                <Button type="submit" className="mt-4">Save Options</Button>
+                <LoadingButton />
             </Form>
         </Container>
     );
