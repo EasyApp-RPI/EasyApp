92484cf66ee374c64cfeafc5e7cf85c577804e95
Yanzhen Lu <jimlo7760@gmail.com>
2023-09-29 17:18:50 -0400

    Revert "Formatting the code, nothing change."
    This reverts commit b6fddfa7eef0346712ffa2a97cad8908ab7e14d2.

--- react-chrome-extension/src/options/App.tsx
+++ react-chrome-extension/src/options/App.tsx
@@ -1,42 +1,42 @@
 // This app is the settings page where users can enter their info, and tweak general options about the extension
 
 import React from 'react';
-import {Container, Form, Button, Dropdown, DropdownButton, ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
-import {useForm} from 'react-hook-form';
+import { Container, Form, Button, Dropdown, DropdownButton, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import {useEffect, useState} from 'react';
 
 
 function LoadingButton() {
     const [isLoading, setLoading] = React.useState(false);
-
+  
     React.useEffect(() => {
-        function simulateNetworkRequest() {
-            return new Promise((resolve) => setTimeout(resolve, 1000));
-        }
-
-        if (isLoading) {
-            simulateNetworkRequest().then(() => {
-                setLoading(false);
-            });
-        }
+      function simulateNetworkRequest() {
+        return new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+  
+      if (isLoading) {
+        simulateNetworkRequest().then(() => {
+          setLoading(false);
+        });
+      }
     }, [isLoading]);
-
+  
     const handleClick = () => setLoading(true);
-
+  
     return (
-        <button
-            className="btn btn-primary"
-            disabled={isLoading}
-            onClick={!isLoading ? handleClick : undefined}
-        >
-            {isLoading ? 'Loading…' : 'Submit'}
-        </button>
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={!isLoading ? handleClick : undefined}
+      >
+        {isLoading ? 'Loading…' : 'Submit'}
+      </button>
     );
-}
+  }
 
 
 function EasyAppOptions() {
-    const {register, handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
 
     const onSubmit = (data: any) => {
         console.log(data);
@@ -45,7 +45,7 @@ function EasyAppOptions() {
     return (
         <Container>
             <h2 className="text-center mb-4">EasyApp Options</h2>
-
+            
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group>
                     <Form.Control type="text" placeholder="Your Name" {...register('name')} />
@@ -58,18 +58,17 @@ function EasyAppOptions() {
                 <Form.Group>
                     <Form.Control as="textarea" placeholder="Your Interests" {...register('interests')} />
                 </Form.Group>
-
+                
                 <Form.Group>
                     <Form.Control as="textarea" placeholder="Skills" {...register('skills')} />
                 </Form.Group>
-
+                
                 <Form.Group>
                     <Form.Control as="textarea" placeholder="Past Jobs/Experiences" {...register('jobs')} />
                 </Form.Group>
 
                 <Form.Group>
-                    <Form.Control as="textarea"
-                                  placeholder="LinkedIn/Github/Personal Websites" {...register('links')} />
+                    <Form.Control as="textarea" placeholder="LinkedIn/Github/Personal Websites" {...register('links')} />
                 </Form.Group>
 
                 <Form.Group>
@@ -91,7 +90,7 @@ function EasyAppOptions() {
                     <Form.Label>Upload CV:</Form.Label>
                     <Form.Control type="file" {...register('cv')} />
                 </Form.Group>
-                <LoadingButton/>
+                <LoadingButton />
             </Form>
         </Container>
     );
