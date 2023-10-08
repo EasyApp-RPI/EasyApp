237cc82f4c6f4a83db2acfef31efcde716912151
Anthony Fabius <afabius05@gmail.com>
2023-10-07 20:46:02 -0400

    Running Prettier on codebase

--- autofill/autofillScript.ts
+++ autofill/autofillScript.ts
@@ -2,7 +2,7 @@
 const usernameField = document.querySelector('input[name="username"]');
 const passwordField = document.querySelector('input[name="password"]');
 
-if (usernameField) (usernameField as HTMLInputElement).value = "yourUsername";
-if (passwordField) (passwordField as HTMLInputElement).value = "yourPassword";
+if (usernameField) (usernameField as HTMLInputElement).value = 'yourUsername';
+if (passwordField) (passwordField as HTMLInputElement).value = 'yourPassword';
 
-export {}
+export {};
