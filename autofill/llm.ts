d5bb68cc0b7923ee04daf1aa0350ce77fd90a500
Samir Beall <samir.beall@gmail.com>
2023-10-10 16:19:46 -0400

    switch to llm

--- autofill/llm.ts
+++ autofill/llm.ts
@@ -1,3 +1,60 @@
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { UserInfo, FieldInfo} from "./types";
+import { LLMChain } from "langchain/chains";
 
-export const chatModel = new ChatOpenAI({openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0});
+export const model = new OpenAI(
+  {openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: "gpt-3.5-turbo-instruct",
+    temperature: 0,
+    stop: ["\n, input label:"]
+  });
+
+
+const prompt= PromptTemplate.fromTemplate(`
+{userInfo}
+
+Given this user information, respond to the input field label and name
+
+input label: First Name
+name: FirstName
+id: first-name
+placeholder: Enter First Name
+
+input text: {firstName}
+
+input label: null
+name: phoneNumber
+id: null
+placeholder: Enter your mobile number here
+
+input text: {phoneNumber}
+
+input label: {inputLabel}
+name: {name}
+id: {id}
+placeholder: {placeholder}
+
+input text:
+`);
+
+
+export const answerField = async (userInfo: UserInfo, fieldInfo: FieldInfo) => {
+  const chain = new LLMChain({ llm: model, prompt });
+
+  let result = chain.call({
+    userInfo: JSON.stringify(userInfo),
+    firstName: userInfo.firstName,
+    phoneNumber: userInfo.phoneNumber,
+    inputLabel: fieldInfo.inputLabel,
+    name: fieldInfo.name,
+    id: fieldInfo.id,
+    placeholder: fieldInfo.placeholder,
+  });
+
+
+  result = result.then((value) => (value.res.text));
+
+  return result
+}
+
---
+++ autofill/types.ts
@@ -0,0 +1,9 @@
+export type UserInfo = any;
+
+export interface FieldInfo {
+  inputLabel: string,
+  name: string,
+  placeholder: string,
+}
+
+
