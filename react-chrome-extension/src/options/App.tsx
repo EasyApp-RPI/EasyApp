4872f91bbe08460a88ef074ded404840c380a878
katijino <powers2@rpi.edu>
2023-10-13 17:41:03 -0400

    Settings Page Storage
    Co-Authored-By:jimlo7760 <jimlo7760@gmail.com>

--- react-chrome-extension/src/options/App.tsx
+++ react-chrome-extension/src/options/App.tsx
@@ -1,62 +1,57 @@
 // This app is the settings page where users can enter their info, and tweak general options about the extension
 
 import React from 'react';
-import {Container, Form, Button, Dropdown, DropdownButton, ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
-import {useForm} from 'react-hook-form';
+import {Container, Form, Button} from 'react-bootstrap';
 import {useEffect, useState} from 'react';
-import {ChangeEvent} from 'react';
-
-
-
   
 function EasyAppOptions() {
 
- 
-    const {register, handleSubmit} = useForm();
-    const [formData, setFormData] = useState({});
-    const [storedName, setStoredName] = useState("");
-    const [storedMajor, setStoredMajor] = useState("");
-    const [storedInterests, setStoredInterests] = useState("");
-    const [storedSkills, setStoredSkills] = useState("");
-    const [storedJobs, setStoredJobs] = useState("");
-    const [storedLinks, setStoredLinks] = useState("");
-    const [storedQuestions, setStoredQuestions] = useState("");
+    // Sets initial state of form fields
+    const [FormData, setFormData] = useState([
+        {key:"name", value:'', label:"Name", type:"input"},
+        {key:"major", value:'', label:"Major", type:"input"},
+        {key:"interests", value:'', label:"Interests", type:"textarea"},
+        {key:"skills", value:'', label:"Skills", type:"textarea"},
+        {key:"experience", value:'', label:"Experience", type:"textarea"},
+        {key:"links", value:'', label:"Links", type:"textarea"},
+        {key:"supplement", value:'', label:"SupplementaryText", type:"textarea"},
+    ]);
 
-
-
-
-
+    // On page load/refresh it loads local storage data into the value of the form field.
     useEffect(() => {
-        chrome.storage.local.get(null, (result) => {
-          console.log("All stored data:", result);
-          
-          const storedName = result.key.name || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredName(storedName);
-          const storedMajor = result.key.major || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredMajor(storedMajor);
-          const storedInterests = result.key.interests || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredInterests(storedInterests);
-          const storedSkills = result.key.skills || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredSkills(storedSkills);
-          const storedJobs = result.key.jobs || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredJobs(storedJobs);
-          const storedLinks = result.key.links || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredLinks(storedLinks);
-          const storedQuestions = result.key.otherQuestions || ""; // Replace 'key.name' with the actual key you want to retrieve
-          setStoredQuestions(storedQuestions);
-        });
-      }, []);
-    const onSubmit = (data: any) => {
-        
-        chrome.storage.local.set({ key: data}).then(() => {
-            console.log("Value is set");
-            console.log(data);
-          });
-    };
+        handleFormDataLoad();
+        handleFileChange;
+    },[])
 
+    // Loads data from local storage into the stateful FormData variable
+    const handleFormDataLoad = () => {
+        FormData.map((data, index) => {
+            const value : any  = localStorage.getItem(data.key);
+            if(value !== null){
+                data.value = value;
+                handleFormDataChange(index, data.value);
+            }
+        })
+    }
+
+    // Updates FormData on change to any form field
+    const handleFormDataChange = (index : number, str : string) => {
+        const updatedData = [...FormData];
+        updatedData[index].value = str;
+        setFormData(updatedData);
+    }
+    // Stores current FormData in the local storage
+    const handleSubmit = () => {
+        FormData.map((data: {key:string, value:string}) => {
+            localStorage.setItem(data.key, data.value.trim())
+        });
+        handleFormDataLoad;
+        console.log(FormData);
+    }
 
     // Limit the size of the file uploaded
     const [errorMessage, setErrorMessage] = useState('');
+
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const selectedFile = event.target.files && event.target.files[0];
@@ -77,56 +72,40 @@ function EasyAppOptions() {
         }
     };
 
+
     return (
         <Container>
             <h2 className="text-center mb-4">EasyApp Options</h2>
 
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Group>
-                    <Form.Control type="text" placeholder="Your Name" value={storedName} {...register('name')} onChange={(e) => setStoredName(e.target.value)} />
-                </Form.Group>
+            <Form>
 
-                <Form.Group>
-                    <Form.Control type="text" placeholder="Your Major" value={storedMajor}  {...register('major')} onChange={(e) => setStoredMajor(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Control as="textarea" placeholder="Your Interests" value={storedInterests} {...register('interests')}onChange={(e) => setStoredInterests(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Control as="textarea" placeholder="Skills"value={storedSkills} {...register('skills')} onChange={(e) => setStoredSkills(e.target.value)}/>
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Control as="textarea" placeholder="Past Jobs/Experiences"value={storedJobs} {...register('jobs')} onChange={(e) => setStoredJobs(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Control as="textarea" placeholder="LinkedIn/Github/Personal Websites"value={storedLinks} {...register('links')} onChange={(e) => setStoredLinks(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Control as="textarea" placeholder="Supplementary Text"value={storedQuestions} {...register('otherQuestions')} onChange={(e) => setStoredQuestions(e.target.value)}/>
-                </Form.Group>
-
+                {FormData.map((data, index) => (
+                    <Form.Group key={index}>
+                        <Form.Control
+                            as = {(index<2)?"input":"textarea"}
+                            placeholder = {data.key}
+                            defaultValue = {data.value}
+                            value = {data.value}
+                            onChange = {(event) => handleFormDataChange(index, event.target.value)}/>
+                    </Form.Group>
+                ))}
 
                 <Form.Group>
                     <Form.Label>Upload Resume:</Form.Label>
-                    <Form.Control type="file" accept=".pdf, .doc, .docx, .tex" {...register('resume')} onChange={handleFileChange}/>
+                    <Form.Control type="file" accept=".pdf, .doc, .docx, .tex" name="Resume" onChange={handleFileChange}/>
                 </Form.Group>
 
                 <Form.Group>
                     <Form.Label>Upload Cover Letter:</Form.Label>
-                    <Form.Control type="file" {...register('coverLetter')} />
+                    <Form.Control type="file" name="Cover Letter" />
                 </Form.Group>
 
                 <Form.Group>
                     <Form.Label>Upload CV:</Form.Label>
-                    <Form.Control type="file" {...register('cv')} />
+                    <Form.Control type="file" name="CV" />
                 </Form.Group>
 
-                 <Button type="submit">Submit</Button>{' '}
+                 <Button onClick={handleSubmit} >Submit</Button>{' '}
             </Form>
         </Container>
     );
