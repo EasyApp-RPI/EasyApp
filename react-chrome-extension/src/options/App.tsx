e2ef0d4872d815b93ace5ffaaf70a9d0118eaeff
katijino <powers2@rpi.edu>
2023-11-10 17:34:56 -0500

    Commented Code
    Commented code for easier understanding of the functions, for anyone trying to understand how to connect IndexedDB to their code.

--- react-chrome-extension/src/options/App.tsx
+++ react-chrome-extension/src/options/App.tsx
@@ -6,93 +6,135 @@ import {useEffect, useState, ChangeEvent} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// openDB function returns a Promise that resolves to an IndexedDB database instance
 const openDB = (): Promise<IDBDatabase> => {
+  // Return a Promise that wraps the logic for opening or upgrading the IndexedDB database
   return new Promise<IDBDatabase>((resolve, reject) => {
+    // Use window.indexedDB to open the 'FilesDB' database with version 1
     const request = window.indexedDB.open('FilesDB', 1);
 
+    // Handle errors that may occur during the attempt to open the database
     request.onerror = (event: Event) => {
       reject('Error opening database');
     };
 
+    // Handle successful opening of the database
     request.onsuccess = (event: Event) => {
+      // Extract the result from the event and cast it to an IDBDatabase instance
       const target = event.target as IDBOpenDBRequest;
       const db = target.result as IDBDatabase;
+
+      // Check if the database instance is valid and resolve the Promise with it
       if (db) {
         resolve(db);
       } else {
+        // Reject the Promise if the database instance is not valid
         reject('Failed to open database');
       }
     };
 
+    // Handle the case where the database version needs an upgrade
     request.onupgradeneeded = (event: Event) => {
+      // Extract the result from the event and cast it to an IDBDatabase instance
       const target = event.target as IDBOpenDBRequest;
       const db = target.result as IDBDatabase;
+
+      // Create an object store named 'files' with 'id' as the key path
       db.createObjectStore('files', { keyPath: 'id' });
     };
   });
 };
-  
-  const saveFile = (file: File, key: string) => {
-    return openDB().then((db) => {
-      return new Promise((resolve, reject) => {
-        removeFile(key) // Remove existing file with the same key
-          .then(() => {
-            const transaction = db.transaction('files', 'readwrite');
-            const objectStore = transaction.objectStore('files');
-            const request = objectStore.add({ id: key, file });
-  
-            request.onsuccess = () => {
-              resolve('File saved successfully');
-            };
-  
-            request.onerror = () => {
-              reject('Error saving file');
-            };
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
+
+// saveFile function saves a file to the 'files' object store in the IndexedDB database
+const saveFile = (file: File, key: string) => {
+  // Use the openDB function to get the database instance in a Promise chain
+  return openDB().then((db) => {
+    // Return a new Promise that wraps the logic for saving the file
+    return new Promise((resolve, reject) => {
+      // Call the removeFile function to remove any existing file with the same key
+      removeFile(key)
+        .then(() => {
+          // Begin a 'readwrite' transaction on the 'files' object store
+          const transaction = db.transaction('files', 'readwrite');
+          // Get the object store within the transaction
+          const objectStore = transaction.objectStore('files');
+          // Use the add method to add a new entry with the specified key and file
+          const request = objectStore.add({ id: key, file });
+
+          // Handle the success event when adding the file is successful
+          request.onsuccess = () => {
+            resolve('File saved successfully');
+          };
+
+          // Handle the error event when adding the file encounters an error
+          request.onerror = () => {
+            reject('Error saving file');
+          };
+        })
+        .catch((error) => {
+          // Reject the Promise if an error occurs during the removeFile operation
+          reject(error);
+        });
     });
-  };
+  });
+};
+
   
   
-  const getAllFiles = () => {
-    return openDB().then((db) => {
-      return new Promise((resolve, reject) => {
-        const transaction = db.transaction('files', 'readonly');
-        const objectStore = transaction.objectStore('files');
-        const request = objectStore.getAll();
-  
-        request.onsuccess = () => {
-          resolve(request.result);
-        };
-  
-        request.onerror = () => {
-          reject('Error getting files');
-        };
-      });
+// getAllFiles function retrieves all files from the 'files' object store in the IndexedDB database
+const getAllFiles = () => {
+  // Use the openDB function to get the database instance in a Promise chain
+  return openDB().then((db) => {
+    // Return a new Promise that wraps the logic for getting all files
+    return new Promise((resolve, reject) => {
+      // Start a 'readonly' transaction on the 'files' object store
+      const transaction = db.transaction('files', 'readonly');
+      // Get the object store within the transaction
+      const objectStore = transaction.objectStore('files');
+      // Use the getAll method to retrieve all files from the object store
+      const request = objectStore.getAll();
+
+      // Handle the success event when retrieving files is successful
+      request.onsuccess = () => {
+        resolve(request.result);
+      };
+
+      // Handle the error event when an error occurs during the retrieval of files
+      request.onerror = () => {
+        reject('Error getting files');
+      };
     });
-  };
+  });
+};
 
 
-  const removeFile = (fileKey: string) => {
-    return openDB().then((db) => {
-      return new Promise<string>((resolve, reject) => {
-        const transaction = db.transaction('files', 'readwrite');
-        const objectStore = transaction.objectStore('files');
-        const request = objectStore.delete(fileKey);
-  
-        request.onsuccess = () => {
-          resolve('File removed successfully');
-        };
-  
-        request.onerror = () => {
-          reject('Error removing file');
-        };
-      });
+
+// removeFile function removes a file from the 'files' object store in the IndexedDB database
+const removeFile = (fileKey: string) => {
+  // Use the openDB function to get the database instance in a Promise chain
+  return openDB().then((db) => {
+    // Return a new Promise that wraps the logic for removing the file
+    return new Promise<string>((resolve, reject) => {
+      // Start a 'readwrite' transaction on the 'files' object store
+      const transaction = db.transaction('files', 'readwrite');
+      // Get the object store within the transaction
+      const objectStore = transaction.objectStore('files');
+      // Use the delete method to remove the file with the specified key from the object store
+      const request = objectStore.delete(fileKey);
+
+      // Handle the success event when removing the file is successful
+      request.onsuccess = () => {
+        resolve('File removed successfully');
+      };
+
+      // Handle the error event when an error occurs during the removal of the file
+      request.onerror = () => {
+        reject('Error removing file');
+      };
     });
-  };
+  });
+};
+
 function EasyAppOptions() {
 
     // Sets initial state of form fields
@@ -220,20 +262,29 @@ function EasyAppOptions() {
         }
     };
 
-    const traverseFileInputs = () => {
-      const fileInputs = document.querySelectorAll('input[type="file"]');
-      console.log(fileInputs);
-    
-      fileInputs.forEach((input) => {
-        if (input instanceof HTMLInputElement && input.files && input.files.length > 0) {
-          const file = input.files[0];
-          const key = input.name; // Assuming the input's name attribute is used as the key
-          saveFile(file, key)
-            .then((message) => console.log(message))
-            .catch((error) => console.error(error));
-        }
-      });
-    };
+// traverseFileInputs function processes file inputs on the document
+const traverseFileInputs = () => {
+  // Select all file input elements on the document
+  const fileInputs = document.querySelectorAll('input[type="file"]');
+  console.log(fileInputs);
+
+  // Iterate over each file input
+  fileInputs.forEach((input) => {
+    // Check if the element is an instance of HTMLInputElement and has files
+    if (input instanceof HTMLInputElement && input.files && input.files.length > 0) {
+      // Get the first file from the input's files array
+      const file = input.files[0];
+      // Use the input's name attribute as the key (assuming it is used as the key)
+      const key = input.name;
+
+      // Call the saveFile function to save the file with the specified key
+      saveFile(file, key)
+        .then((message) => console.log(message))
+        .catch((error) => console.error(error));
+    }
+  });
+};
+
       const handleRetrieveFiles = async () => {
         const files = await getAllFiles();
         console.log(files); // You can handle these files as needed (e.g., display in the UI)
@@ -243,18 +294,29 @@ function EasyAppOptions() {
         handleFileChange;
     }, [])
 
-    const handleSubmit = () => {
-        traverseFileInputs();
-        let dataToSave: { [key: string]: string } = {};
-        FormData.forEach((data) => {
-            dataToSave[data.key] = data.value.trim();
-        });
+// handleSubmit function handles the submission of form data
+const handleSubmit = () => {
+  // Traverse file inputs to save files to IndexedDB
+  traverseFileInputs();
 
-        chrome.storage.sync.set(dataToSave, function () {
-            console.log('Data saved to chrome.storage.sync.');
-            handleFormDataLoad();
-        });
-    }
+  // Initialize an object to store form data
+  let dataToSave: { [key: string]: string } = {};
+
+  // Iterate over form data and trim values
+  FormData.forEach((data) => {
+    dataToSave[data.key] = data.value.trim();
+  });
+
+  // Save the form data to chrome.storage.sync
+  chrome.storage.sync.set(dataToSave, function () {
+    // Log a success message to the console
+    console.log('Data saved to chrome.storage.sync.');
+
+    // Trigger a function to handle loading form data (assuming it is defined)
+    handleFormDataLoad();
+  });
+};
+
 
 
         return (
