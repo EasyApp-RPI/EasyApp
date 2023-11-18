9c6dee2b18fb730a099a26412c950b77bb56c4a1
mik0lam <michaellamcc2004@gmail.com>
2023-11-17 21:08:52 -0500

    Cover Letter
    Changed up the cover letter parse, is now a stable prototype, has name body and address sections.
    
    Co-Authored-By: MBtheOtaku <45703141+MBtheOtaku@users.noreply.github.com>
    Co-Authored-By: hli2238 <121896329+hli2238@users.noreply.github.com>

--- PDFParse/coverParse.js
+++ PDFParse/coverParse.js
@@ -1,15 +1,92 @@
-function parseCoverLetter(coverLetter) {
-    // Regular expression to match the name, assuming it ends with a comma
-    var nameRegex = /^(.*?),/;
-    // Find the name using the regular expression
-    var nameMatch = coverLetter.match(nameRegex);
-    var name = nameMatch ? nameMatch[1].trim() : '';
-    // Remove the name part from the cover letter to get the body
-    var body = coverLetter.replace(nameRegex, '').trim();
-    return { name: name, body: body };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var fs = require('fs');
+var pdf = require('pdf-parse');
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+function parseCoverLetterFromPdf(pdfPath) {
+    var _this = this;
+    return new Promise(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+        var dataBuffer, data, pdfText, regex, match, name_1, address, body, outputJson, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    dataBuffer = fs.readFileSync(pdfPath);
+                    return [4 /*yield*/, pdf(dataBuffer)];
+                case 1:
+                    data = _a.sent();
+                    pdfText = data.text;
+                    regex = /([^]+?)(\b\d{1,5}(?:[-\s]\d{1,5})?(?:[A-Za-z]+\b|\b)[^]+?)([\s\S]*)/;
+                    match = pdfText.match(regex);
+                    if (!match) {
+                        console.error('Unable to extract name, address, and body from PDF.');
+                        resolve(null);
+                    }
+                    name_1 = match[1].replace(/[\n\r]/g, ' ').trim();
+                    address = match[2].replace(/[\n\r]/g, ' ').trim();
+                    body = match[3].trim();
+                    outputJson = JSON.stringify({ name: name_1, address: address, body: body });
+                    resolve(outputJson);
+                    return [3 /*break*/, 3];
+                case 2:
+                    error_1 = _a.sent();
+                    console.error('Error parsing PDF:', error_1.message);
+                    reject(error_1);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    }); });
 }
-// Sample cover letter
-var sampleCoverLetter = "\n  John Doe,\n  \n  I am writing to express my interest in the Software Developer position at your company. \n  ...\n  Thank you for considering my application.\n\n  Sincerely,\n  John Doe\n";
-var result = parseCoverLetter(sampleCoverLetter);
-console.log('Name:', result.name);
-console.log('Body:', result.body);
+// Ask the user for the PDF file path
+rl.question('Enter the path to the PDF file: ', function (pdfPath) {
+    // Parse cover letter from PDF
+    parseCoverLetterFromPdf(pdfPath)
+        .then(function (outputJson) {
+        // Output the result
+        console.log(outputJson);
+        rl.close();
+    })
+        .catch(function (error) {
+        console.error(error);
+        rl.close();
+    });
+});
--- PDFParse/coverParse.ts
+++ PDFParse/coverParse.ts
@@ -1,30 +1,61 @@
-function parseCoverLetter(coverLetter) {
-  // Regular expression to match the name, assuming it ends with a comma
-  const nameRegex = /^(.*?),/;
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const readline = require('readline');
 
-  // Find the name using the regular expression
-  const nameMatch = coverLetter.match(nameRegex);
-  const name = nameMatch ? nameMatch[1].trim() : '';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  // Remove the name part from the cover letter to get the body
-  const body = coverLetter.replace(nameRegex, '').trim();
+function parseCoverLetterFromPdf(pdfPath) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Read the PDF file
+      const dataBuffer = fs.readFileSync(pdfPath);
 
-  return { name, body };
+      // Parse the PDF
+      const data = await pdf(dataBuffer);
+
+      // Extract text content from the PDF
+      const pdfText = data.text;
+
+      // Regular expression to match the name, address, and body
+      const regex = /([^]+?)(\b\d{1,5}(?:[-\s]\d{1,5})?(?:[A-Za-z]+\b|\b)[^]+?)([\s\S]*)/;
+
+      // Find matches using the regular expression
+      const match = pdfText.match(regex);
+
+      if (!match) {
+        console.error('Unable to extract name, address, and body from PDF.');
+        resolve(null);
+      }
+
+      // Extract the name, address, and body
+      const name = match[1].replace(/[\n\r]/g, ' ').trim();
+      const address = match[2].replace(/[\n\r]/g, ' ').trim();
+      const body = match[3].trim();
+
+      // Create and return the output JSON
+      const outputJson = JSON.stringify({ name, address, body });
+      resolve(outputJson);
+    } catch (error) {
+      console.error('Error parsing PDF:', error.message);
+      reject(error);
+    }
+  });
 }
 
-// Sample cover letter
-const sampleCoverLetter = `
-  John Doe,
-  
-  I am writing to express my interest in the Software Developer position at your company. 
-  ...
-  Thank you for considering my application.
-
-  Sincerely,
-  John Doe
-`;
-
-const result = parseCoverLetter(sampleCoverLetter);
-
-console.log('Name:', result.name);
-console.log('Body:', result.body);
+// Ask the user for the PDF file path
+rl.question('Enter the path to the PDF file: ', (pdfPath) => {
+  // Parse cover letter from PDF
+  parseCoverLetterFromPdf(pdfPath)
+    .then(outputJson => {
+      // Output the result
+      console.log(outputJson);
+      rl.close();
+    })
+    .catch(error => {
+      console.error(error);
+      rl.close();
+    });
+});
