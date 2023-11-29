6b53e028cc2fc087c183a675f1bf87c1cbdb00cd
Anthony Fabius <afabius05@gmail.com>
2023-11-29 02:12:42 -0500

    Files & Personal Comp Implementation
    I added the files for the expected number of components we are going to have. I also began working on the personal information component. I got it working so the rest should really just be a copy and paste

---
+++ react-chrome-extension/src/options/SkillsComp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useEffect, useState} from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default function SkillsComp(){
+
+    return (
+      <Container>
+        <h1>Skills Component Heading!</h1>
+      </Container>
+    )
+}
---
+++ react-chrome-extension/src/options/WorkComp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useEffect, useState} from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default function WorkComp(){
+
+    return (
+      <Container>
+        <h1>Work Component Heading!</h1>
+      </Container>
+    )
+}
