ad5f2d0406a5f37aa62fc74f3eda53e0d1a08ddb
Anthony Fabius <afabius05@gmail.com>
2023-11-29 23:02:37 -0500

    Working Education and Work Experience
    More blood sweat and tears but a little less time :)

--- react-chrome-extension/src/options/SkillsComp.tsx
+++ react-chrome-extension/src/options/SkillsComp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
-import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import {Row, Col, Form, Button} from 'react-bootstrap';
 
 export default function SkillsComp({info, setInfo}){
 
