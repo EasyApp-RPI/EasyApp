fe62fa8b4882cff29133812146dbf499229bb886
Tripplenut <Tripplenut@users.noreply.github.com>
2023-11-30 04:03:19 +0000

    Prettified Code!
    Automated with github actions

--- react-chrome-extension/src/options/PersonalComp.tsx
+++ react-chrome-extension/src/options/PersonalComp.tsx
@@ -1,86 +1,61 @@
 import { any, number } from 'prop-types';
 import React from 'react';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { Container, Row, Col, Form } from 'react-bootstrap';
 
-export default function PersonalComp({info, setInfo}){
-
+export default function PersonalComp({ info, setInfo }) {
   const handleDataChange = (index, str) => {
     const updatedData = [...info];
     updatedData[index].value = str;
     setInfo(updatedData);
-  }
+  };
 
-  function Field(key){
-    return(
+  function Field(key) {
+    return (
       <Form.Group>
         <Form.Control // OpenAi Key
-        as = {info[key].type}
-        placeholder = {info[key].key}
-        defaultValue = {info[key].value}
-        value = {info[key].value}
-        onChange = {(event) => handleDataChange(key, event.target.value)}
+          as={info[key].type}
+          placeholder={info[key].key}
+          defaultValue={info[key].value}
+          value={info[key].value}
+          onChange={(event) => handleDataChange(key, event.target.value)}
         />
       </Form.Group>
-    )
+    );
   }
 
   return (
     <>
       <h2 className='text-center mb-3'>Personal Information</h2>
+      <Row>{Field(0) /* OpenAi Key */}</Row>
+
       <Row>
-        {Field(0) /* OpenAi Key */}
+        <Col>{Field(1) /* First Name */}</Col>
+
+        <Col>{Field(2) /* Middle Name */}</Col>
+
+        <Col>{Field(3) /* Last Name */}</Col>
       </Row>
 
       <Row>
-        <Col>
-          {Field(1) /* First Name */}
-        </Col>
+        <Col>{Field(4) /* Email */}</Col>
 
-        <Col>
-          {Field(2) /* Middle Name */ }
-        </Col>
+        <Col>{Field(5) /* Mobile Phone Number */}</Col>
 
-        <Col>
-          {Field(3) /* Last Name */}
-        </Col>
+        <Col>{Field(6) /* Work Phone Number */}</Col>
       </Row>
 
-      <Row>
-        <Col>
-          {Field(4) /* Email */}
-        </Col>
-
-        <Col>
-          {Field(5) /* Mobile Phone Number */ }
-        </Col>
-
-        <Col>
-          {Field(6) /* Work Phone Number */}
-        </Col>
-      </Row>
+      <Row>{Field(7) /* Address */}</Row>
 
       <Row>
-        {Field(7) /* Address */} 
-      </Row>
+        <Col>{Field(8) /* Country */}</Col>
 
-      <Row>
-        <Col>
-          {Field(8) /* Country */}
-        </Col>
+        <Col>{Field(9) /* State */}</Col>
 
-        <Col>
-          {Field(9) /* State */}
-        </Col>
+        <Col>{Field(10) /* City */}</Col>
 
-        <Col>
-          {Field(10) /* City */}
-        </Col>
-
-        <Col>
-          {Field(11) /* Zip Code */}
-        </Col>
+        <Col>{Field(11) /* Zip Code */}</Col>
       </Row>
     </>
-  )
+  );
 }
--- react-chrome-extension/src/options/SkillsComp.tsx
+++ react-chrome-extension/src/options/SkillsComp.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import {useState} from 'react';
-import {Row, Col, Form, Button} from 'react-bootstrap';
+import { useState } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 
-export default function SkillsComp({info, setInfo}){
-
+export default function SkillsComp({ info, setInfo }) {
   const handleDataChange = (index, str) => {
     const updatedData = [...info];
     updatedData[index].value = str;
     setInfo(updatedData);
-  }
+  };
 
   const handleDataAdd = () => {
-    setInfo([...info, { key: 'Skill', value: '', label: 'Skill', type:'input', required: false }]);
+    setInfo([
+      ...info,
+      {
+        key: 'Skill',
+        value: '',
+        label: 'Skill',
+        type: 'input',
+        required: false,
+      },
+    ]);
   };
 
   const handleRemoveExperience = (index) => {
@@ -20,42 +28,44 @@ export default function SkillsComp({info, setInfo}){
     setInfo(updatedExperience);
   };
 
-  function Field(key){
-    return(
+  function Field(key) {
+    return (
       <Row>
         <Col>
           <Form.Group>
-            <Form.Control 
-            as = {info[key].type}
-            placeholder = {info[key].key}
-            defaultValue = {info[key].value}
-            value = {info[key].value}
-            onChange = {(event) => handleDataChange(key, event.target.value)}
+            <Form.Control
+              as={info[key].type}
+              placeholder={info[key].key}
+              defaultValue={info[key].value}
+              value={info[key].value}
+              onChange={(event) => handleDataChange(key, event.target.value)}
             />
           </Form.Group>
         </Col>
-        <Col xs="auto">
+        <Col xs='auto'>
           <Button variant='danger' onClick={() => handleRemoveExperience(key)}>
             Delete
           </Button>
         </Col>
       </Row>
-    )
+    );
   }
 
   return (
     <>
       <Row>
-        <Col xs="auto">
-          <Button variant='success' onClick={handleDataAdd}>Add</Button>
-        </Col >
+        <Col xs='auto'>
+          <Button variant='success' onClick={handleDataAdd}>
+            Add
+          </Button>
+        </Col>
         <Col>
           <h2 className='text-center mb-3'>Skills</h2>
         </Col>
       </Row>
-      {info.map((data,index) => {
+      {info.map((data, index) => {
         return Field(index);
       })}
     </>
-  )
+  );
 }
