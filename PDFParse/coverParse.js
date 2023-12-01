dab3de7f23dd6e82cba323443a072657dc482650
hli2238 <hwli2238@gmail.com>
2023-12-01 17:17:22 -0500

    Database in cover letter parser
    Modified the code to also implement the db function in the cover letter parser so the code can take in a file from the database instead of a manual inputted string
    
    Co-Authored-By: mik0lam <144414594+mik0lam@users.noreply.github.com>
    Co-Authored-By: MBtheOtaku <45703141+MBtheOtaku@users.noreply.github.com>

--- PDFParse/coverParse.js
+++ PDFParse/coverParse.js
@@ -13,7 +13,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+        while (_) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -34,6 +34,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var _this = this;
 var fs = require('fs');
 var pdf = require('pdf-parse');
 var readline = require('readline');
@@ -41,51 +42,112 @@ var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-function parseCoverLetterFromPdf(pdfPath) {
-    var _this = this;
-    return new Promise(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
-        var dataBuffer, data, pdfText, regex, match, name_1, address, body, outputJson, error_1;
+var openDB = function () {
+    return new Promise(function (resolve, reject) {
+        var request = window.indexedDB.open('FilesDB', 1);
+        request.onerror = function (event) {
+            reject('Error opening database');
+        };
+        request.onsuccess = function (event) {
+            var target = event.target;
+            var db = target.result;
+            if (db) {
+                resolve(db);
+            }
+            else {
+                reject('Failed to open database');
+            }
+        };
+        request.onupgradeneeded = function (event) {
+            var target = event.target;
+            var db = target.result;
+            db.createObjectStore('files', { keyPath: 'id' });
+        };
+    });
+};
+var parseCoverLetterFromPdf = function (pdfPath) { return __awaiter(_this, void 0, void 0, function () {
+    var dataBuffer, data, pdfText, regex, match, name_1, address, body, outputJson, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                dataBuffer = fs.readFileSync(pdfPath);
+                return [4 /*yield*/, pdf(dataBuffer)];
+            case 1:
+                data = _a.sent();
+                pdfText = data.text;
+                regex = /([^]+?)(\b\d{1,5}(?:[-\s]\d{1,5})?(?:[A-Za-z]+\b|\b)[^]+?)([\s\S]*)/;
+                match = pdfText.match(regex);
+                if (!match) {
+                    console.error('Unable to extract name, address, and body from PDF.');
+                    return [2 /*return*/];
+                }
+                name_1 = match[1].replace(/[\n\r]/g, ' ').trim();
+                address = match[2].replace(/[\n\r]/g, ' ').trim();
+                body = match[3].trim();
+                outputJson = { name: name_1, address: address, body: body };
+                // Save the parsed data to IndexedDB
+                //await savePdfDataToDB('coverLetter', Buffer.from(JSON.stringify(outputJson)));
+                console.log('Cover letter data saved to IndexedDB.');
+                return [3 /*break*/, 3];
+            case 2:
+                error_1 = _a.sent();
+                console.error('Error parsing PDF:', error_1.message);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
+function getPdfDataFromDB() {
+    return __awaiter(this, void 0, void 0, function () {
+        var db, transaction, objectStore, request_1, error_2;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     _a.trys.push([0, 2, , 3]);
-                    dataBuffer = fs.readFileSync(pdfPath);
-                    return [4 /*yield*/, pdf(dataBuffer)];
+                    return [4 /*yield*/, openDB()];
                 case 1:
-                    data = _a.sent();
-                    pdfText = data.text;
-                    regex = /([^]+?)(\b\d{1,5}(?:[-\s]\d{1,5})?(?:[A-Za-z]+\b|\b)[^]+?)([\s\S]*)/;
-                    match = pdfText.match(regex);
-                    if (!match) {
-                        console.error('Unable to extract name, address, and body from PDF.');
-                        resolve(null);
-                    }
-                    name_1 = match[1].replace(/[\n\r]/g, ' ').trim();
-                    address = match[2].replace(/[\n\r]/g, ' ').trim();
-                    body = match[3].trim();
-                    outputJson = JSON.stringify({ name: name_1, address: address, body: body });
-                    resolve(outputJson);
-                    return [3 /*break*/, 3];
+                    db = _a.sent();
+                    transaction = db.transaction('files', 'readonly');
+                    objectStore = transaction.objectStore('files');
+                    request_1 = objectStore.get('coverLetter');
+                    return [2 /*return*/, new Promise(function (resolve, reject) {
+                            request_1.onsuccess = function () {
+                                var result = request_1.result;
+                                if (result && result.data instanceof Uint8Array) {
+                                    resolve(result.data);
+                                }
+                                else {
+                                    resolve(null);
+                                }
+                            };
+                            request_1.onerror = function () {
+                                console.error('Error fetching data from database');
+                                resolve(null);
+                            };
+                        })];
                 case 2:
-                    error_1 = _a.sent();
-                    console.error('Error parsing PDF:', error_1.message);
-                    reject(error_1);
-                    return [3 /*break*/, 3];
+                    error_2 = _a.sent();
+                    console.error('Error opening database:', error_2.message);
+                    return [2 /*return*/, null];
                 case 3: return [2 /*return*/];
             }
         });
-    }); });
+    });
 }
 // Ask the user for the PDF file path
 rl.question('Enter the path to the PDF file: ', function (pdfPath) {
     // Parse cover letter from PDF
     parseCoverLetterFromPdf(pdfPath)
+        .then(function () {
+        // Retrieve cover letter data from IndexedDB
+        return getPdfDataFromDB();
+    })
         .then(function (outputJson) {
         // Output the result
-        console.log(outputJson);
+        console.log('Retrieved data from IndexedDB:', outputJson);
         rl.close();
-    })
-        .catch(function (error) {
+    })["catch"](function (error) {
         console.error(error);
         rl.close();
     });
--- PDFParse/coverParse.ts
+++ PDFParse/coverParse.ts
@@ -7,54 +7,113 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function parseCoverLetterFromPdf(pdfPath) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Read the PDF file
-      const dataBuffer = fs.readFileSync(pdfPath);
+interface ParsedCoverLetter {
+  name: string;
+  address: string;
+  body: string;
+}
 
-      // Parse the PDF
-      const data = await pdf(dataBuffer);
+const openDB = (): Promise<IDBDatabase> => {
+  return new Promise<IDBDatabase>((resolve, reject) => {
+    const request = window.indexedDB.open('FilesDB', 1);
 
-      // Extract text content from the PDF
-      const pdfText = data.text;
+    request.onerror = (event: Event) => {
+      reject('Error opening database');
+    };
 
-      // Regular expression to match the name, address, and body
-      const regex = /([^]+?)(\b\d{1,5}(?:[-\s]\d{1,5})?(?:[A-Za-z]+\b|\b)[^]+?)([\s\S]*)/;
+    request.onsuccess = (event: Event) => {
+      const target = event.target as IDBOpenDBRequest;
+      const db = target.result as IDBDatabase;
 
-      // Find matches using the regular expression
-      const match = pdfText.match(regex);
-
-      if (!match) {
-        console.error('Unable to extract name, address, and body from PDF.');
-        resolve(null);
+      if (db) {
+        resolve(db);
+      } else {
+        reject('Failed to open database');
       }
+    };
 
-      // Extract the name, address, and body
-      const name = match[1].replace(/[\n\r]/g, ' ').trim();
-      const address = match[2].replace(/[\n\r]/g, ' ').trim();
-      const body = match[3].trim();
+    request.onupgradeneeded = (event: Event) => {
+      const target = event.target as IDBOpenDBRequest;
+      const db = target.result as IDBDatabase;
 
-      // Create and return the output JSON
-      const outputJson = JSON.stringify({ name, address, body });
-      resolve(outputJson);
-    } catch (error) {
-      console.error('Error parsing PDF:', error.message);
-      reject(error);
-    }
+      db.createObjectStore('files', { keyPath: 'id' });
+    };
   });
+};
+
+const parseCoverLetterFromPdf = async (pdfPath: string): Promise<void> => {
+  try {
+    const dataBuffer = fs.readFileSync(pdfPath);
+    const data = await pdf(dataBuffer);
+
+    const pdfText = data.text;
+
+    const regex = /([^]+?)(\b\d{1,5}(?:[-\s]\d{1,5})?(?:[A-Za-z]+\b|\b)[^]+?)([\s\S]*)/;
+    const match = pdfText.match(regex);
+
+    if (!match) {
+      console.error('Unable to extract name, address, and body from PDF.');
+      return;
+    }
+
+    const name = match[1].replace(/[\n\r]/g, ' ').trim();
+    const address = match[2].replace(/[\n\r]/g, ' ').trim();
+    const body = match[3].trim();
+
+    const outputJson: ParsedCoverLetter = { name, address, body };
+
+    // Save the parsed data to IndexedDB
+    //await savePdfDataToDB('coverLetter', Buffer.from(JSON.stringify(outputJson)));
+
+    console.log('Cover letter data saved to IndexedDB.');
+  } catch (error) {
+    console.error('Error parsing PDF:', error.message);
+  }
+};
+
+async function getPdfDataFromDB(): Promise<Uint8Array | null> {
+  try {
+    const db = await openDB();
+    const transaction = db.transaction('files', 'readonly');
+    const objectStore = transaction.objectStore('files');
+    const request = objectStore.get('coverLetter');
+
+    return new Promise<Uint8Array | null>((resolve, reject) => {
+      request.onsuccess = () => {
+        const result = request.result;
+
+        if (result && result.data instanceof Uint8Array) {
+          resolve(result.data);
+        } else {
+          resolve(null);
+        }
+      };
+
+      request.onerror = () => {
+        console.error('Error fetching data from database');
+        resolve(null);
+      };
+    });
+  } catch (error) {
+    console.error('Error opening database:', error.message);
+    return null;
+  }
 }
 
 // Ask the user for the PDF file path
 rl.question('Enter the path to the PDF file: ', (pdfPath) => {
   // Parse cover letter from PDF
   parseCoverLetterFromPdf(pdfPath)
-    .then(outputJson => {
+    .then(() => {
+      // Retrieve cover letter data from IndexedDB
+      return getPdfDataFromDB();
+    })
+    .then((outputJson) => {
       // Output the result
-      console.log(outputJson);
+      console.log('Retrieved data from IndexedDB:', outputJson);
       rl.close();
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
       rl.close();
     });
